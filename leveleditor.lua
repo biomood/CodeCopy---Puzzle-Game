789302ceae6cd1e583664f68054a4cc2ed522610
David Roberts <biomood@gmail.com>
2011-06-18 22:45:37 +0100

    Method declarations for the level editor

--- leveleditor.lua
+++ leveleditor.lua
@@ -1,2 +1,49 @@
 module(..., package.seeall);
 
+-----------------------------------
+-- Level editor update functions --
+-----------------------------------
+
+-- update the editor
+function updateLevelEditor(dt)
+end
+
+-- update the level menu
+function updateLevelMenu(dt)
+end
+
+-- update the current user level in play
+function updateUserLevel()
+end
+
+---------------------------------
+-- Level editor draw functions --
+---------------------------------
+
+-- draw the level editor
+function drawLevelEditor()
+end
+
+-- draw the select menu level
+function drawLevelMenu()
+end
+
+-- draw the user level in play
+function drawUserLevel()
+end
+
+--------------------------
+-- Level file functions --
+--------------------------
+
+-- save the current level in editor with level name
+function saveLevel(levelName)
+end
+
+-- load the level with given level name
+function loadLevel(levelName)
+end
+
+-- return a list of all user created levels
+function getAllLevels()
+end
